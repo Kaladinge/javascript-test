129880a0a97b006e9501690417ae2400be4e5d42
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2020-09-22 11:01:28 +0200

    Added to js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // This is my first JavaScript code
 
-let name;
+let name = "Lars-Inge";
 console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain space or hyphen (-)
+// Are case-sensitive
+
+let firstName = "Lars-Inge";
+let lastName = "Johnsen";
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
